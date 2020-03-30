0bbedfc7ca8f82b2f979f31adf6db2348abe3020
Andreas Heige <a.heige.83@gmail.com>
2020-03-30 21:47:29 +0200

    added files

---
+++ front-end/src/services/registration/__mocks__/index.js
@@ -0,0 +1,11 @@
+import { reject } from "q"
+
+export default {
+    register (detail) {
+        return new Promise((resolve, reject) => {
+            detail.emailAddress === 'sunny@local'
+            ? resolve({result: 'success'})
+            :reject(new Error('User already exist'))
+        })
+    }
+}

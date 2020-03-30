aeaa08a126e82610bbfc00f0c84d42c724718e37
Andreas Heige <a.heige.83@gmail.com>
2020-03-30 21:57:36 +0200

    updates

--- src/test/java/com/TaskAgileApplicationTests.java
+++ src/test/java/com/TaskAgileApplicationTests.java
@@ -1,13 +1,17 @@
-package com.taskagile;
+package com;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootTest(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class TaskAgileApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }

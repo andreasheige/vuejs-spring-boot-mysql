746039a5448959a52603be33d2173e7c4dc55917
Andreas Heige <a.heige.83@gmail.com>
2020-04-01 22:09:59 +0200

    add spring security

---
+++ src/main/java/com/taskagile/config/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.taskagile.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    private static final String[] PUBLIC = new String[]{
+        "/error", "/login", "/logout", "/register", "/api/registrations"};
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers(PUBLIC).permitAll()
+            .anyRequest().authenticated()
+           .and()
+            .formLogin()
+            .loginPage("/login")
+           .and()
+            .logout()
+            .logoutUrl("/logout")
+           .and()
+            .csrf().disable();
+    }
+    
+    @Override
+    public void configure(WebSecurity web){
+        web.ignoring().antMatchers("/static/**", "/js/**", "/css/**", "/images/**", "/favicon.ico");
+    }
+
+}
--- src/test/java/com/taskagile/web/apis/RegistrationApiControllerTests.java
+++ src/test/java/com/taskagile/web/apis/RegistrationApiControllerTests.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.taskagile.config.SecurityConfiguration;
 import com.taskagile.domain.application.UserService;
 import com.taskagile.domain.model.user.EmailAddressExistsException;
 import com.taskagile.domain.model.user.UsernameExistsException;
@@ -18,11 +19,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(RegistrationApiController.class)
+@ContextConfiguration(classes = {SecurityConfiguration.class, RegistrationApiController.class})
+@WebMvcTest
 public class RegistrationApiControllerTests {
 
   @Autowired

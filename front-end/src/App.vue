b7831444d0ecfab78417b68978cc4d88fdb5f990
Andreas Heige <a.heige.83@gmail.com>
2020-04-03 11:21:21 +0200

    added login page etc

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -14,13 +14,30 @@ export default {
 html {
   font-size: 14px;
 }
-.form-group {
+
+.public-container {
+  max-width: 900px;
+}
+.public{
+  .form{
+    margin-top: 50px;
+    width: 320px;
+
+  .form-group {
+    label {
+      font-weight: bold;
+      color: #555;
+    }
+
   .error {
     line-height: 1;
     display: none;
     margin-top: 5px;
   }
+  }
+  }
 }
+
 .field-error {
   .error {
     display: block;
---
+++ front-end/src/components/PageFooter.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="footer">
+    <span class="copyright">&copy; 2020 Andreas Heige</span>
+    <ul class="footer-links list-inline float-right">
+        <li class="list-inline-item"><a href="#">About</a></li>
+        <li class="list-inline-item"><a href="#">Terms of Servicce</a></li>
+        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+        <li class="list-inline-item"><a href="https://github.com/andreasheige" target="_blank">Andreas Heige @ Github</a></li>
+    </ul>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'PageFooter'
+}
+</script>
+
+<style lang="scss" scooped>
+.footer{
+    width: 100%;
+    font-size: 13px;
+    color: #666;
+    line-height: 40px;
+    border-top: 1px solid #ddd;
+    margin-top: 50px;
+
+    .list-inline-kitem{
+        margin-right: 10px;
+    }
+
+    a{
+        color: #666;
+    }
+}
+
+</style>
